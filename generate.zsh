4bb99281b4352fb5f5d2333bd8d3ac8780ac09e8
Damien Gasparina <d.gasparina@gmail.com>
2018-08-28 18:06:02 +0200

    first commit

---
+++ generate.zsh
@@ -0,0 +1,58 @@
+#!/bin/zsh
+
+OUT=kafka.zsh
+COMMANDS=("kafka-acls" "kafka-avro-console-consumer" "kafka-avro-console-producer"
+"kafka-broker-api-versions" "kafka-configs" "kafka-console-consumer" 
+"kafka-console-producer" "kafka-consumer-groups" "kafka-consumer-perf-test" 
+"kafka-delegation-tokens" "kafka-topics"
+)
+
+function kafka_retrieve_help_command() {
+	cmd=$1
+	option=""
+	desc=""
+	help_output=`$cmd 2>&1`
+	arg_name="$(echo $cmd | tr - _)_args"
+
+	echo "declare -a $arg_name"  >> $OUT
+	echo "$arg_name=()" >> $OUT
+
+	for word in `echo $help_output`; do
+		if [[ $word =~ ^--[a-z\-]+ ]] && [[ $word =~ [a-z]+ ]]; then
+			if [ ! -z $option ]; then
+				echo "$arg_name+=('$option:${desc//\'/''}')" >> $OUT
+			fi	
+
+			option=$word
+			desc=""
+		else
+			desc="$desc $word"
+		fi
+	done
+
+	if [ ! -z $option ]; then
+		echo "$arg_name+=('$option:${desc//\'/''}')" >> $OUT
+	fi	
+}
+
+function kafka-command() {
+	cmd=$1
+	echo "compdef \"_kafka-command $cmd\" $cmd" >> $OUT
+}
+
+cat << EOF > $OUT
+#!/bin/sh
+
+function _kafka-command() {
+	cmd=\$1
+	arg_name="\$(echo \$cmd | tr - _)_args"
+	typeset -a options
+	eval _describe 'values' options -- \$arg_name
+}
+
+EOF
+
+for cmd in ${COMMANDS[@]}; do
+	kafka_retrieve_help_command $cmd
+	kafka-command $cmd
+done
